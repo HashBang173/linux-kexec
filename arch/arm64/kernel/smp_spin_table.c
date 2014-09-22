af48154a2ee4ea626aaad80b93752ee85609b8d5
Geoff Levand <geoff@infradead.org>
2014-09-22 10:33:40 -0700

    arm64: Rework spin-table code
    Rework the spin-table enable method to properly support CPU hot
    plug and kexec re-boot.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- arch/arm64/kernel/smp_spin_table.c
+++ arch/arm64/kernel/smp_spin_table.c
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define DEBUG
 
 #include <linux/delay.h>
 #include <linux/init.h>
@@ -26,14 +27,21 @@
 #include <asm/cpu_ops.h>
 #include <asm/cputype.h>
 #include <asm/smp_plat.h>
+#include <asm/system_misc.h>
 
 #include "cpu-properties.h"
 
+static const char method_name[] = "spin-table";
+
 extern void secondary_holding_pen(void);
+
 volatile unsigned long secondary_holding_pen_release = INVALID_HWID;
+volatile unsigned long secondary_holding_pen_return = INVALID_ADDR;
+volatile unsigned long secondary_holding_pen_count = 0;
 
 static DEFINE_PER_CPU(struct cpu_properties, cpu_properties) = {
 	.cpu_release_addr = INVALID_ADDR,
+	.cpu_return_addr = INVALID_ADDR,
 };
 
 /*
@@ -121,12 +129,107 @@ static int smp_spin_table_cpu_boot(unsigned int cpu)
 	return 0;
 }
 
+
+static int smp_spin_table_cpu_disable(unsigned int cpu)
+{
+	struct cpu_properties *p = &per_cpu(cpu_properties, cpu);
+
+	return (p->cpu_return_addr == INVALID_ADDR) ? -EOPNOTSUPP : 0;
+}
+
+static void smp_spin_table_cpu_die(unsigned int cpu)
+{
+	const struct cpu_properties *p = &per_cpu(cpu_properties, cpu);
+
+	/* Send cpu back to spin-table. */
+
+	local_disable(DAIF_ALL);
+	soft_restart(p->cpu_release_addr);
+
+	BUG();
+}
+
 const struct cpu_operations smp_spin_table_ops = {
-	.name		= "spin-table",
+	.name		= method_name,
 	.cpu_init	= smp_spin_table_cpu_init,
 	.cpu_prepare	= smp_spin_table_cpu_prepare,
 	.cpu_boot	= smp_spin_table_cpu_boot,
+	.cpu_disable	= smp_spin_table_cpu_disable,
+	.cpu_die	= smp_spin_table_cpu_die,
 };
 
+void smp_spin_table_shutdown(void)
+{
+	struct device_node *dn;
+
+	pr_devel("%s: ->\n", __func__);
+	pr_devel("%s:%d: secondary_holding_pen_count: %lu\n", __func__,
+		__LINE__, secondary_holding_pen_count);
+
+	/*
+	 * Empty the secondary_holding_pen by releasing all cpus described in
+	 * the device tree.  Contiune the loop if any errors are encountered.
+	 */
+
+	for (dn = NULL; (dn = of_find_node_by_type(dn, "cpu")); ) {
+		struct cpu_properties cp;
+		unsigned long timeout;
+		unsigned long trigger;
+
+		if (read_cpu_properties(&cp, dn))
+			continue;
+
+		if (cp.hwid == cpu_logical_map(smp_processor_id()))
+			continue;
+
+		if (strcmp(cp.enable_method, method_name))
+			continue;
+
+		/* For debugging. */
+
+		if (cp.cpu_return_addr == 1)
+			cp.cpu_return_addr = (u64)secondary_holding_pen;
+
+		trigger = secondary_holding_pen_count - 1;
+
+		mb();
+
+		secondary_holding_pen_return = cp.cpu_return_addr;
+
+		__flush_dcache_area((void *)&secondary_holding_pen_return,
+			sizeof(secondary_holding_pen_return));
+
+		secondary_holding_pen_release = cp.hwid;
+
+		__flush_dcache_area((void *)&secondary_holding_pen_release,
+			sizeof(secondary_holding_pen_release));
+
+		sev();
+
+		/* Wait for cpu to leave the secondary_holding_pen. */
+
+		for (timeout = 1000;
+			timeout && secondary_holding_pen_count > trigger;
+			timeout--) {
+			udelay(10);
+			mb();
+		}
+
+		if (!timeout)
+			pr_warn("%s: hwid %llx: shutdown timeout\n", __func__,
+				cp.hwid);
+	}
+
+	pr_devel("%s:%d: secondary_holding_pen_count: %lu\n", __func__,
+		__LINE__, secondary_holding_pen_count);
+
+	if (secondary_holding_pen_count)
+		pr_warn("%s: Warning: Some cpus may be lost (%lu).\n",
+			__func__, secondary_holding_pen_count);
+
+	pr_devel("%s: <-\n", __func__);
+}
+
 const struct cpu_operation_method cpu_operation_spin_table = {
+	.shutdown = smp_spin_table_shutdown,
 };
